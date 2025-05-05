20c00d720bd296905d4168025e522e877acccd04
Kevin Mendoza <143244057+Keevvvssss@users.noreply.github.com>
2025-05-05 10:22:16 +0800

    Update Client_Tracking.blade.php
    Update Dashboard Route, and other connections.

--- system/resources/views/livewire/welcome/Client_Tracking.blade.php
+++ system/resources/views/livewire/welcome/Client_Tracking.blade.php
@@ -128,7 +128,7 @@
                         <ul>
                             <!-- Dashboard -->
                             <li class="mb-4 mt-[28px]">
-                                <a href="#" class="flex items-center space-x-4 p-2 group transition duration-200">
+                                <a href="{{ route('cdashboard') }}" class="flex items-center space-x-4 p-2 group transition duration-200">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-7 h-7 text-red-800 group-hover:text-red-800" viewBox="0 0 24 24">
                                         <path d="M3 3h8v8H3V3zm10 0h8v8h-8V3zm0 10h8v8h-8v-8zm-10 0h8v8H3v-8z" />
                                     </svg>
@@ -144,7 +144,7 @@
                                 </a></li>
                             <!-- Tracking -->
                             <li class="mb-4">
-                                <a href="#" class="flex items-center space-x-4 p-2 border-l-4 border-red-800 group transition duration-200">
+                                <a href="{{ route('Ctracking') }}" class="flex items-center space-x-4 p-2 border-l-4 border-red-800 group transition duration-200">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-7 h-7 text-red-800 group-hover:text-red-800">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1 0 4.5 4.5a7.5 7.5 0 0 0 12.15 12.15z" />
                                     </svg>
