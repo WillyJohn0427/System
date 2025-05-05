a5936cff84da316a1df01b0d4572d2e1f0e7230b
Kevin Mendoza <143244057+Keevvvssss@users.noreply.github.com>
2025-05-05 11:10:58 +0800

    Update DocuFolderb5.blade.php
    Update bin and logout space/location.

--- system/resources/views/livewire/welcome/DocuFolderb5.blade.php
+++ system/resources/views/livewire/welcome/DocuFolderb5.blade.php
@@ -203,10 +203,10 @@
                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2" />
                 </svg>
                 <span class="text-red-800 text-[19px] group-hover:text-red-800 group-hover:text-[20px] group-hover:font-bold transition-all duration-200">Bin</span>
-                </a> 
+                </a>  
                 <!-- Log Out Button Trigger -->
                 <button onclick="openLogoutModal()" 
-                    class="absolute inset-x-6 bottom-[43px] ml-2 flex items-center space-x-4 p-2 border-l-4 border-transparent group transition duration-200 cursor-pointer">
+                    class="absolute inset-x-6 bottom-[50px] ml-2 flex items-center space-x-4 p-2 border-l-4 border-transparent group transition duration-200 cursor-pointer">
                     <svg xmlns="http://www.w3.org/2000/svg"
                         fill="none" viewBox="0 0 24 24" stroke-width="2" 
                         stroke="currentColor" class="w-8 h-8 text-red-800 group-hover:text-red-800">
@@ -237,7 +237,7 @@
                 </form>
                 </div>
                 </div>
-                </div>                     
+                </div>                  
             </aside>
 
             <!-- Search Bar Below Header -->
